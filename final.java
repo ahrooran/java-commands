68a3d5a0b43ab9ea0b54c42fe3f046584faba741
ahrooran <ahrooran@hotmail.co.uk>
2019-06-05 15:03:34 +0100

    final

---
+++ final.java
@@ -0,0 +1,21 @@
+final class tss{
+	public void msg(){
+		System.out.println("hello");
+	}
+	public void message(){
+		System.out.println("hello again");
+	}
+}
+
+class london extends tss{
+	public void msg(){
+		System.out.prinln("123");
+	}
+}
+
+class final{
+	public static void main(String args[]){
+		
+	}
+
+}
